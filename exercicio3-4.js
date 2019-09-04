3dfc50483c6bf9596df5334988da0a4ce78d62b1
niluiz <nicoleduarteluna@hotmail.com>
2019-09-04 20:39:59 -0300

    Exercício 3

---
+++ exercicio3-4.js
@@ -0,0 +1,10 @@
+var numero = 2;
+var fParImpar = function (num) {
+    return num % 2;
+};
+if (fParImpar(numero) == 0) {
+    console.log("Esse n\u00FAmero \u00E9 par: " + numero);
+}
+else {
+    console.log("Esse n\u00FAmero \u00E9 \u00EDmpar: " + numero);
+}
---
+++ exercicio3-4.ts
@@ -0,0 +1,10 @@
+let numero:number =2
+let fParImpar = function (num:number) :number {
+    return num%2;
+}  
+    if (fParImpar(numero) == 0) {
+        console.log (`Esse número é par: ${numero}`)
+    }
+    else {
+        console.log (`Esse número é ímpar: ${numero}`)
+    }
