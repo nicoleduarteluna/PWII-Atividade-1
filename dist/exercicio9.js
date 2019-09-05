696a911717c1dd2af8f171e09d5de8415eb62281
niluiz <nicoleduarteluna@hotmail.com>
2019-09-04 22:34:53 -0300

    Exercício 9

---
+++ dist/exercicio9.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.fParImpar = function (num) {
+    return num % 2 == 0;
+};
+exports.fArea = function (larg, comp) {
+    return (larg * comp);
+};
+exports.cubo = function (numero) { return numero * numero * numero; };
---
+++ exercicio9.ts
@@ -0,0 +1,9 @@
+export let fParImpar = function (num:number) : boolean {
+    return num % 2 == 0
+} 
+
+export let fArea = function (larg:number, comp:number) : number {
+    return (larg * comp)
+}
+
+export let cubo =  (numero:number) : number => numero * numero * numero
