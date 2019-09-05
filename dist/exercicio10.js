a76b4071c9ffaec75c64b08c3686d5a4a194c81e
niluiz <nicoleduarteluna@hotmail.com>
2019-09-04 22:37:43 -0300

    Exercício 10

---
+++ dist/exercicio10.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var exercicio9_1 = require("./exercicio9");
+var numero = 10;
+console.log("O cubo do n\u00FAmero \u00E9: " + exercicio9_1.cubo(numero));
---
+++ exercicio10.ts
@@ -0,0 +1,4 @@
+import {cubo} from "./exercicio9"
+
+let numero:number = 10;
+console.log(`O cubo do número é: ${cubo(numero)}`);
