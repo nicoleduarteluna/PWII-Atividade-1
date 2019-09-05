122ff72c0284edda4ff76b65e14bd533d46c00fe
niluiz <nicoleduarteluna@hotmail.com>
2019-09-04 21:35:46 -0300

    Exercício 5

---
+++ exercicio5.js
@@ -0,0 +1,6 @@
+var largura = 250;
+var comprimento = 500;
+var fArea = function (larg, comp) {
+    return (larg * comp);
+};
+console.log("A \u00E1rea da figura \u00E9 " + fArea(largura, comprimento) + ", com comprimento " + comprimento + " e largura " + largura + ".");
---
+++ exercicio5.ts
@@ -0,0 +1,8 @@
+let largura: number = 250
+let comprimento: number = 500
+
+let fArea = function (larg:number, comp:number) : number {
+    return (larg * comp)
+}
+
+console.log (`A área da figura é ${fArea(largura,comprimento)}, com comprimento ${comprimento} e largura ${largura}.`)
